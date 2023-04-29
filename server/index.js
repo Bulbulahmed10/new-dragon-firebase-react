d839cc85cb3fde841bdbfe3477b79ad0c6fe17a6
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-04-29 22:04:34 +0600

    create node server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port: http://localhost:${PORT}`);
+});

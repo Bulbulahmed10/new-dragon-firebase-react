7a8be6cde065b9f15e57e9128c7f78a705cb87e0
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-04-30 20:16:53 +0600

    desktop screen desing complete

--- client/src/components/highLightNewsCard/HighLightNewsCard.jsx
+++ client/src/components/highLightNewsCard/HighLightNewsCard.jsx
@@ -5,15 +5,23 @@ import moment from "moment";
 const HighLightNewsCard = ({ singleCategoryNewsData }) => {
   const { _id, image_url, title, category_name, published_date } =
     singleCategoryNewsData;
+
   return (
-    <div className="border p-2 mb-4 rounded-sm">
-      <img className="mb-2" src={image_url} alt="" />
+    <div className="border flex flex-col justify-between p-2 mb-4 rounded-sm ">
+      <img
+        className="mb-2 w-[270px] h-[140px] object-cover rounded-sm"
+        src={image_url}
+        alt=""
+      />
       <Link
         to={`/news/${_id}`}
         className="font-bold text-[#403F3F] hover:text-blue-500">
         {title.slice(0, 80)}...
       </Link>
-      <div className="flex items-center justify-between gap-4 mt-4 border-t py-1">
+      <div
+        className={`${
+          category_name && "flex"
+        } items-center justify-between gap-4 mt-4 border-t py-1`}>
         <span className="capitalize font-semibold text-sm text-blue-400">
           {category_name}
         </span>
--- client/src/components/newsDetails/NewsDetails.jsx
+++ client/src/components/newsDetails/NewsDetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import RightNav from "../../shared/rightNav/RightNav";
 import { Link, useParams } from "react-router-dom";
 import { BsArrowLeft } from "react-icons/bs";
+import HighLightNewsCard from "../highLightNewsCard/HighLightNewsCard";
 const NewsDetails = () => {
   const [newsData, setNewsData] = useState(null);
   const params = useParams();
@@ -14,26 +15,48 @@ const NewsDetails = () => {
   const findData =
     newsData && newsData.find((singleNews) => singleNews._id === params.id);
 
+  for (let i = newsData && newsData.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [newsData[i], newsData[j]] = [newsData[j], newsData[i]];
+  }
+
   return (
     <div className="flex gap-6 mt-8">
-      <div className="basis-4/5 p-4 border rounded-sm h-fit">
-        <img
-          className="rounded-md w-full h-[500px] object-full"
-          src={findData && findData?.image_url}
-          alt=""
-        />
-        <h2 className="my-4 text-2xl font-bold text-[#403F3F]">
-          {findData && findData?.title}
-        </h2>
-        <p className="text-lg text-[#706F6F]">{findData && findData.details}</p>
-        <Link
-          to={`/categories/${parseInt(findData && findData?.category_id)}`}
-          className="flex items-center gap-2 bg-[#D72050] w-fit text-white px-4 py-2 font-semibold text-lg mt-4 rounded-md">
-          <BsArrowLeft className="text-xl" />{" "}
-          <span>All news in this category</span>
-        </Link>
+      <div className="basis-4/5">
+        <div className="p-4 border rounded-sm h-fit">
+          <img
+            className="rounded-md w-full h-[500px] object-cover"
+            src={findData && findData?.image_url}
+            alt=""
+          />
+          <h2 className="my-4 text-2xl font-bold text-[#403F3F]">
+            {findData && findData?.title}
+          </h2>
+          <p className="text-lg text-[#706F6F]">
+            {findData && findData.details}
+          </p>
+          <Link
+            to={`/categories/${parseInt(findData && findData?.category_id)}`}
+            className="flex items-center gap-2 bg-[#D72050] w-fit text-white px-4 py-2 font-semibold text-lg mt-4 rounded-md">
+            <BsArrowLeft className="text-xl" />{" "}
+            <span>All news in this category</span>
+          </Link>
+        </div>
+        <div className="grid grid-cols-4 justify-between mt-6 gap-2">
+          {newsData &&
+            newsData
+              .slice(0, 4)
+              .map((singleNews) => (
+                <HighLightNewsCard
+                  key={singleNews._id}
+                  singleCategoryNewsData={singleNews}
+                />
+              ))}
+        </div>
       </div>
-      <RightNav />
+      <div className="basis-1/5">
+        <RightNav />
+      </div>
     </div>
   );
 };
